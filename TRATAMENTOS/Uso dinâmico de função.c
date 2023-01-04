e7c489d1e1cdd5194428d15682857a3c66cb41b8
Allen <114815898+AllenoBoy@users.noreply.github.com>
2023-01-03 22:28:07 -0300

    Create Uso dinâmico de função.c

---
+++ TRATAMENTOS/Uso dinâmico de função.c
@@ -0,0 +1,26 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <locale.h>
+
+// DECLARAÇÃO DA FUNÇÃO
+   void limparthing (void);
+
+
+int main () {
+
+    setlocale(LC_ALL, "");
+
+    // USO DA FUNÇÃO NO INT MAIN
+    limparthing();
+
+
+    return 0;
+
+            }
+
+
+// ORDENAÇÃO NA FUNÇÃO
+void limparthing (void) {
+
+    printf("\n Seu nome é robson? ");
+                        }
